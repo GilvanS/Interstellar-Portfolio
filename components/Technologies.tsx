8ad60cf8967b7f2961d7e59db951ceca1d209bff
GilvanJSSousa <gillvanjs@gmail.com>
2025-12-23 17:25:15 -0300

    certificado

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -128,6 +128,16 @@ const allCertificates: Certificate[] = [
     imageUrl: '/certificates/DIO -GFT Quality Assurance #1_ZBMZETMN.png',
     platform: 'dio'
   },
+  {
+    name: 'Introdução ao Desenvolvimento Moderno de Software',
+    imageUrl: '/certificates/Introdução ao Desenvolvimento Moderno de Software_9XXGKSU1.png',
+    platform: 'dio'
+  },
+  {
+    name: 'Testes funcionais de aplicações Android com Appium',
+    imageUrl: '/certificates/Testes funcionais de aplicações Android com Appium_UC-4f6b53eb-7358-4a86-bb31-e1da94401d60.png',
+    platform: 'dio'
+  },
 ];
 
 const technologies = [
